303d7130283f0021cea947a6c32236d6f1de2c32
MatchaOnMuffins <matcha@rchat.fun>
2022-11-11 08:19:08 +0000

    Modify arguments

--- src/main.c
+++ src/main.c
@@ -4,28 +4,14 @@
 #include <stdbool.h>
 #include <string.h>
 
+#define RELEASE "1.0.1"
+
 int main(int argc, char *argv[])
 {
-    // if --version is passed, print version and exit
-    if (argc > 1 && strcmp(argv[1], "--version") == 0)
-    {
-        printf("%s", curl_version());
-        return 0;
-    }
-    // if -a is passed, let all = true
-    bool all = false;
-    if (argc > 1 && strcmp(argv[1], "-a") == 0)
-    {
-        all = true;
-    }
-    // if -l is passed, location = true
 
-    bool location = false;
-    if (argc > 1 && strcmp(argv[1], "-l") == 0)
-    {
-        location = true;
-    }
+    // if -a is passed, let all = true
     // if -c is passed, country = true
+    // if -l is passed, location = true
     // if -r is passed, region = true
     // if -o is passed, organization = true
     // if -t is passed, timezone = true
@@ -33,11 +19,39 @@ int main(int argc, char *argv[])
     // if -h is passed, hostnames = true
     // if -ct is passed, city = true
     // if --help is passed, print help and exit
+    // if --version is passed, print version and exit
 
-    if (argc > 1 && strcmp(argv[1], "--help") == 0)
+    // if flags are passed but doesnt match any of the above, print error and exit
+    bool all = false;
+    bool location = false;
+    bool country = false;
+    bool region = false;
+    bool organization = false;
+    bool timezone = false;
+    bool postal = false;
+    bool hostnames = false;
+    bool city = false;
+    bool noargs = false;
+    if (argc == 2)
     {
-        printf("Usage: %s [OPTION]", argv[0]);
-        printf(" \
+        if (strcmp(argv[1], "--version") == 0)
+        {
+            printf("MyIP Version: %s\n", RELEASE);
+            printf("CURL Version: %s\n", curl_version());
+            return 0;
+        }
+        else if (strcmp(argv[1], "-a") == 0)
+        {
+            all = true;
+        }
+        else if (strcmp(argv[1], "-l") == 0)
+        {
+            location = true;
+        }
+        else if (strcmp(argv[1], "--help") == 0)
+        {
+            printf("Usage: %s [OPTION]", argv[0]);
+            printf(" \
 -a: all | \
 -l: location | \
 -c: country | \
@@ -48,43 +62,53 @@ int main(int argc, char *argv[])
 -h: hostnames | \
 -ct: city | \
 --help | \
---version");
-        return 0;
+--version\n");
+            return 0;
+        }
+        else if (strcmp(argv[1], "-c") == 0)
+        {
+            country = true;
+        }
+        else if (strcmp(argv[1], "-r") == 0)
+        {
+            region = true;
+        }
+        else if (strcmp(argv[1], "-o") == 0)
+        {
+            organization = true;
+        }
+        else if (strcmp(argv[1], "-t") == 0)
+        {
+            timezone = true;
+        }
+        else if (strcmp(argv[1], "-p") == 0)
+        {
+            postal = true;
+        }
+        else if (strcmp(argv[1], "-h") == 0)
+        {
+            hostnames = true;
+        }
+        else if (strcmp(argv[1], "-ct") == 0)
+        {
+            city = true;
+        }
+        else
+        {
+            printf("Invalid argument: %s\n", argv[1]);
+            printf("Type %s --help for help\n", argv[0]);
+            return 1;
+        }
     }
-    bool country = false;
-    if (argc > 1 && strcmp(argv[1], "-c") == 0)
+    else if (argc > 2)
     {
-        country = true;
+        printf("Too many arguments\n");
+        printf("Type %s --help for help\n", argv[0]);
+        return 1;
     }
-    bool region = false;
-    if (argc > 1 && strcmp(argv[1], "-r") == 0)
+    else
     {
-        region = true;
-    }
-    bool organization = false;
-    if (argc > 1 && strcmp(argv[1], "-o") == 0)
-    {
-        organization = true;
-    }
-    bool timezone = false;
-    if (argc > 1 && strcmp(argv[1], "-t") == 0)
-    {
-        timezone = true;
-    }
-    bool postal = false;
-    if (argc > 1 && strcmp(argv[1], "-p") == 0)
-    {
-        postal = true;
-    }
-    bool hostnames = false;
-    if (argc > 1 && strcmp(argv[1], "-h") == 0)
-    {
-        hostnames = true;
-    }
-    bool city = false;
-    if (argc > 1 && strcmp(argv[1], "-ct") == 0)
-    {
-        city = true;
+        noargs = true;
     }
 
     CURL *curl;
@@ -131,22 +155,28 @@ int main(int argc, char *argv[])
         {
             curl_easy_setopt(curl, CURLOPT_URL, "https://ipinfo.io/city");
         }
-        else
+        else if (noargs)
         {
             curl_easy_setopt(curl, CURLOPT_URL, "https://ipinfo.io/ip");
         }
-
-        /* Perform the request, res will get the return code */
-        res = curl_easy_perform(curl);
-        /* Check for errors */
-        if (res != CURLE_OK)
-            fprintf(stderr, "curl_easy_perform() failed: %s\n",
-                    curl_easy_strerror(res));
-
-        /* always cleanup */
-        curl_easy_cleanup(curl);
     }
 
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+
+    // flush stdout
+    if (noargs || all)
+    {
+        printf("\n");
+    }
+
+    /* Check for errors */
+    if (res != CURLE_OK)
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(res));
+
+    /* always cleanup */
+    curl_easy_cleanup(curl);
     curl_global_cleanup();
     return 0;
 }
